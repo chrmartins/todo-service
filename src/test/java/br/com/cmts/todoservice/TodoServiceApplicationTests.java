39e93f9e334b7ce6aefcaaf33531f6dd2ed21f36
Christian Martins <cmts2010@gmail.com>
2023-08-10 17:23:38 -0300

    feat: initial

---
+++ src/test/java/br/com/cmts/todoservice/TodoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cmts.todoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
